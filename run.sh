018b23903b30c7efc63eb315952ad0d78b114121
Alexandra Negoita <93153280+AlexandraNegoita@users.noreply.github.com>
2023-03-16 16:41:22 +0200

    tests

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ dir="$(dirname $0)"
 function run_test {
     rm -rf output
     pwd
-    filename="$dir/../devoir-1-tests/tests/$1"
+    filename="$dir/devoir-1-tests/tests/$1"
     outputname="$(dirname $filename)/$(basename $1 .a).out"
     echo Running $filename
     # node "$dir/../index.js" "$(cat $filename)" > output
